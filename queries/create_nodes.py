8a0f72746e56b9413b3dba259c1f687b439ef27b
jortiz16 <jortiz16@uw.edu>
2015-05-29 15:48:18 -0700

    adding nodes computation

--- queries/create_nodes.py
+++ queries/create_nodes.py
@@ -1,4 +1,5 @@
 import sys, os, json
+import time
 lib_path = os.path.abspath(os.path.join('../libs/raco', '../libs/myria-python'))
 sys.path.append(lib_path)
 from raco.catalog import FromFileCatalog
@@ -11,7 +12,7 @@ from myria import MyriaSchema
 from myria import MyriaRelation
 
 #CONFIGURE: information about the datasets in Myria
-SNAPSHOT_LIST=['000045' '000054' '000072']
+SNAPSHOT_LIST=['000045','000054','000072']
 USER_NAME="jortiz"
 PROGRAM_NAME="romulustest"
 
@@ -24,10 +25,63 @@ NON_GRP_PARTICLES = '-1'
 EXTRA_ATTRIBUTES = []
 #END CONFIGURE
 
-connection = MyriaConnection(hostname = "rest.myria.cs.washington.edu", port=1776, ssl=True)
+connection = MyriaConnection(hostname = "rest.myria.cs.washington.edu", port=1776, ssl=True, execution_url="https://myria-web.appspot.com")
 
+time_count = 1
+union_string = None
+
+relation_name_prefix = USER_NAME + ":" + PROGRAM_NAME + ":"
 #Write the union
 for i in SNAPSHOT_LIST:
-	#dbscan query
+	#dbscan query?
+	current_relation_name =  relation_name_prefix + "cosmo" + i;
+	#after totalParticles, would have extra attributes
+	current_snapshot = "(select s1.grp as grpID," + str(time_count) + "as currentTime, sum(s1.mass) as mass, count(*) as totalParticles from \""+ current_relation_name + "\" as s1 where s1.grp >" + NON_GRP_PARTICLES + " group by s1.grp)"
+	if(union_string):
+		union_string = union_string + " UNION " + current_snapshot
+	else:
+		union_string = current_snapshot
+	time_count = time_count + 1
+
+#prepare local_nodes schema (if needed, add more attributes in this section)
+
+#write the union at postgres layer (from template query) for local_nodes
+with open('local_nodes.json', 'r+') as data_file:    
+    local_nodes_json_query = json.load(data_file)
+    data_file.close()
+
+#make modifications
+local_nodes_json_query['plan']['fragments'][0]['operators'][0]['sql'] = union_string
+local_nodes_json_query['plan']['fragments'][0]['operators'][1]['relationKey']['userName'] = USER_NAME
+local_nodes_json_query['plan']['fragments'][0]['operators'][1]['relationKey']['programName'] = PROGRAM_NAME
+local_nodes_json_query['plan']['fragments'][0]['operators'][1]['relationKey']['relationName'] = 'nodesLocal'
+
+#run local query
+print "RUNNING LOCAL NODES"
+query_status= connection.submit_query(local_nodes_json_query)
+query_id = query_status['queryId']
+status = (connection.get_query_status(query_id))['status']
+
+#keep checking, sleep a little
+while status!='SUCCESS':
+	status = (connection.get_query_status(query_id))['status']
+	time.sleep(2);
+print "SUCCESS"
 
 #global myrial query
+print "RUNNING GLOBAL NODES"
+global_nodes_query = "T1 = [from scan(" + relation_name_prefix + "nodesLocal) as n emit grpID, currentTime, sum(mass)*" + format(DM_SOL_UNIT, '.1f') + " as mass, sum(totalParticles) as totalParticles]; store(T1, " +  relation_name_prefix +  "haloTable);"
+print global_nodes_query
+query_status = connection.execute_program(program=global_nodes_query)
+query_id = query_status['queryId']
+status = (connection.get_query_status(query_id))['status']
+
+#keep checking, sleep a little
+while status!='SUCCESS':
+	status = (connection.get_query_status(query_id))['status']
+	time.sleep(5);
+print "SUCCESS"
+
+
+
+
